51fa48b52f91283106c218d76f46e51a970f055c
Seonhyuk <gomduridkstjsgur@gmail.com>
2022-06-29 12:46:42 +0900

    update 0629

---
+++ 1725/1725.py
@@ -0,0 +1,33 @@
+import sys
+sys.stdin = open("input.txt")
+
+from collections import deque
+
+n = int(input())
+histogram = deque()
+for _ in range(n):
+    histogram.append(int(sys.stdin.readline()))
+
+stack = deque()
+result = 0
+
+for i in range(len(histogram)):
+    while stack and histogram[stack[-1]] > histogram[i]:
+        a = stack.pop()
+        if stack:
+            solution = (i - stack[-1] - 1) * histogram[a]
+        else:
+            solution = i * histogram[a]
+        result = max(result, solution)
+
+    stack.append(i)
+
+while stack:
+    a = stack.pop()
+    if stack:
+        solution = (len(histogram) - stack[-1] - 1) * histogram[a]
+    else:
+        solution = histogram[a] * len(histogram)
+    result = max(result, solution)
+
+print(result)
